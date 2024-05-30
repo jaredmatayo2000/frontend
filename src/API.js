9b7b23201f51bc6fa9fcdff0045b3c27d766e9e8
JARED MATAYO <77388302+jaredmatayo2000@users.noreply.github.com>
2024-05-30 11:32:55 +0300

    Update API.js

--- src/API.js
+++ src/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://127.0.0.1:8000/backend_api/drugs",
+    baseURL: "http://127.0.0.1:8000/backend_api/lands",
     headers: {
         'Accept':'application/json',
         'Content-Type':'application/json',
